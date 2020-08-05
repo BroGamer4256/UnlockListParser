213f48dbf310089f0eb8ad3c95890f82bf34fe14
BroGamer <iuka@tutanota.com>
2020-08-05 09:40:25 +0100

    Add Vocaloid Room Unlock to reading

--- Writing.cs
+++ Writing.cs
@@ -10,10 +10,32 @@ public class HexWrite
     public static void Write()
     {
         CMNData();
+        VocaData();
     }
     public static void VocaData()
     {
-        
+        var doc = new XmlDocument();
+		doc.Load(@"unlock_list\\VocaRoomUnlock.xml");
+        var VocaRoomReadNode = new List<dynamic>();
+		var VocaRoomReadData = new List<dynamic>();
+		foreach (var node in doc.DocumentElement.ChildNodes)
+		{
+			VocaRoomReadNode.Add(node);
+		}
+        string[] xmlData = {};
+        for (int i = 0; i < VocaRoomReadNode.Count; i++)
+		{
+			Array.Resize(ref xmlData, xmlData.Length + 1);
+			xmlData[i] = VocaRoomReadNode[i].InnerXml;
+			string[] xmlDataSplit = xmlData[i].Split(new string[] { "<", "/<", ">" }, StringSplitOptions.None);
+            var TBA = new VocaRoomUnlock() 
+			{
+				bvVocaloid = Convert.ToInt32(xmlDataSplit[2]),
+                bvSongId = Convert.ToInt32(xmlDataSplit[6]),
+                bvScc = Convert.ToInt32(xmlDataSplit[10])
+			};
+            VocaRoomReadData.Add(TBA); 
+		}
     }
     public static void CMNData()
     {
@@ -46,6 +68,7 @@ public class HexWrite
 				cuUnk08 = Convert.ToInt32(xmlDataSplit[42]), 
 				cuUnk09 = Convert.ToInt32(xmlDataSplit[46])
 			};
+            CMNReadData.Add(TBA);
 		}
     }
 }
